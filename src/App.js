bcf0da63e8e0cc83295bc24175dfa3646e1a5d7c
Sawaira1314 <sawaira.bahadur4@gmail.com>
2024-07-19 12:15:44 +0500

    Added app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Buttonmodule from './button.module.css';
 
 function App() {
+ 
+
+  let [status, setStatus] = useState(false);
+  let [pstatus, setPstatus] = useState(false)
+  let [menustatus, setMenustatus] = useState(false)
+ let [modalstatus,setModalStatus] =useState(false)
+  // else{
+  //   template=<button className='' onClick={() => setpshow(!pshow)}>show</button>
+  // 
+
+  // let n=10;
+  // let displayData = () => {
+  //   seCount(count+1)
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    // <><p> welcome to ws </p>
+  
+      // <><p> welcome to ws </p>
+    
+      <div className="App">
+      <div className="faqouter">
+        <div className="faqitems">
+          <h2>'Popular Articles'</h2>
+          <p>The next step is designing your FAQ page. We have some handy tips for doing this located in the next section of this post. But, if you can't wait until then, here's a jump link so you can skip on down.</p>
+        </div>
+
+        <div className="faqitems">
+          <h2>'Popular Articles'</h2>
+          <p>The next step is designing your FAQ page. We have some handy tips for doing this located in the next section of this post. But, if you can't wait until then, here's a jump link so you can skip on down.</p>
+        </div>
+
+        <div className="faqitems">
+          <h2>'Popular Articles'</h2>
+          <p>The next step is designing your FAQ page. We have some handy tips for doing this located in the next section of this post. But, if you can't wait until then, here's a jump link so you can skip on down.</p>
+        </div>
+
+        <div className="faqitems">
+          <h2>'Popular Articles'</h2>
+          <p>The next step is designing your FAQ page. We have some handy tips for doing this located in the next section of this post. But, if you can't wait until then, here's a jump link so you can skip on down.</p>
+        </div>
+        
+      </div>
+     <div>
+      
+      <h1>
+        FAQ'S
+      </h1>
+     </div>
+
+   <button  className='en' onClick={()=> setModalStatus(true) }> Enquire now</button>
+   <div onclick={()=>setModalStatus(false)} className={`modalOverLay ${modalstatus ? 'modalshow' : ' '}`}>  </div>
+    <div className={`ModalDiv ${modalstatus ? 'showModalDiv' : ' '}` }>
+      <h3>enquire now <span onclick={()=>setModalStatus(false)}>&times;</span></h3>
+   
+   </div>
+
+        
+        <button className='micon' onClick={() => setMenustatus(!menustatus)}>
+        {menustatus ? <span> &times;</span> : <span>&#9776;</span>}
+        </button>
+        <div className={`menu ${menustatus ? 'activeMenu' : ''}`}>
+       
+        
+        <ul>
+
+          <li>home</li>
+          <li>about</li>
+          <li>course</li>
+          <li>gallery</li>
+          <li>contact us</li>
+        </ul>
+      </div>
+      <input type={pstatus ? 'text' : 'password'} /><button onClick={() => setPstatus(!pstatus)}>
+        {(pstatus) ? "hide" : "show"}
+      </button><br /><button className={Buttonmodule.error}> Error</button><button onClick={() => setStatus(!status)}>
+        {(status) ? 'hide' : 'show'}</button>
+      {(status) ? <p className='para'> welcome to my world</p> :  ''}
+    
+      {/* {count}
+      {template}
+    <button onClick>add data</button> */}
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/data/faqquestion.jsx
@@ -0,0 +1,21 @@
+export const question=[
+    {
+      id: 1,
+      question: 'Popular Articles',
+      answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quos nobis nostrum quasi atque' ,    }  ,
+    {
+       id:2,
+       question:'fix problem and request removal',
+       answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quos nobis nostrum quasi atque,
+    } ,
+    {
+       id:3,
+       question:'Browse the web',
+       answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quos nobis nostrum quasi atque,
+    },
+   {
+       id: 4,
+       question: 'search on your phone or tablet',
+       answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quos nobis nostrum quasi atque,
+   }
+   ]
